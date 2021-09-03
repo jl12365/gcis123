b2a243070043630d7ea9c70f461785e30773ff66
jl12365 <89880719+jl12365@users.noreply.github.com>
2021-09-03 16:53:05 -0400

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,32 @@
+def area_circle():
+    radius = int(input('enter radius'))
+    area_circle = (radius * π^2)
+    print('the area of the circle is:', area_circle)
+
+area_circle()
+
+def volume_sphere():
+    radius = int(input('enter_radius'))
+    volume_sphere = ((4/3) * π * r^3)
+    print('the volume of the sphere is:', volume_sphere)
+
+volume_sphere()
+
+def area_rectangle():
+    width = int(input('enter width'))
+    height = int(input('enter height'))
+    area_rectangle = (width * height)
+    print('the area of the rectangle is:', area_rectangle)
+
+area_rectangle()
+
+def area_square():
+    side_length = int(input('enter side length'))
+    area_square = (side_length * side_length)
+    print('the area of the square is:', area_square)
+
+area_square()
+
+def area_isosceles():
+    
+
